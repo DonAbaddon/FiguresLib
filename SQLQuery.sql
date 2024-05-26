3dc129822d096acbd8b164ad6f04080bf53af430
Arthur Tatulyan <101785085+DonAbaddon@users.noreply.github.com>
2024-05-26 22:55:37 +0400

    Обновлено решение задания №3

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,1 +1,4 @@
-SELECT product.name as 'Product', category.name as 'Category' FROM product LEFT JOIN product_category ON product.id = product_category.id_product LEFT JOIN category ON category.id = product_category.id_category;
+SELECT product.name as 'Product', category.name as 'Category' 
+  FROM product 
+  LEFT JOIN product_category ON product.id = product_category.id_product 
+  LEFT JOIN category ON category.id = product_category.id_category;
